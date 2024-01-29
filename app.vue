9a67b8e21e39ebde878aeb483683c35271bf92d9
dpdd <senseosoft@gmail.com>
2024-01-29 21:16:44 +0100

    design

--- app.vue
+++ app.vue
@@ -2,15 +2,15 @@
   <div class="w-full flex h-[100dvh] justify-center items-center bg-pink-600">
     <article class="flex flex-col justify-center items-center">
       <header>
-        <h1 class="text-pink-100 text-xl mb-2">Turbo Noodle</h1>
+        <h1 class="text-pink-100 text-xl mb-2">Turbo Noodle 😻</h1>
       </header>
-      <NuxtImg src="/images/turbie.jpg" />
+      <NuxtImg src="/images/turbie.jpg" class="border border-pink-400 rounded-lg shadow-lg shadow-pink-900" />
     </article>
   </div>
 </template>
 <script setup>
 useHead({
-  title: 'Turbo Noodle - One of the few noodles that is turbo',
+  title: 'Turbo Noodle 🐈 One of the few noodles that is turbo',
   meta: [
     {
       hid: 'description',
