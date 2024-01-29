adea0123aad9c2356873a38de8d71042e01249f1
dpdd <senseosoft@gmail.com>
2024-01-29 20:30:36 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full flex h-[100dvh] justify-center items-center bg-pink-600">
+    <article class="flex flex-col justify-center items-center">
+      <header>
+        <h1 class="text-pink-100 text-xl mb-2">Turbo Noodle</h1>
+      </header>
+      <NuxtImg src="/images/turbie.jpg" />
+    </article>
+  </div>
+</template>
